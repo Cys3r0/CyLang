07963a3584535a6fe215dc02c8992c9b036c5880
Cys3r0 <cysero.inc@gmail.com>
2025-10-11 19:24:36 +0200

    finished basic String functions

--- scanner.c
+++ scanner.c
@@ -5,7 +5,18 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+enum Token {
+    ID,
+    NUM,
+    ASSIGN,
+    PLUS,
+    LPAR,
+    RPAR,
+    LBRACKET,
+    RBRACKET,
 
+
+};
 
 typedef struct {
     char * str;
@@ -13,10 +24,9 @@ typedef struct {
     int len;
 } String;
 
-//TODO: test if BOTH string_add functions work
 //TODO: 
-//TODO: set up github for compiler project.
-
+// Test if BOTH string_add functions work
+// Set up enums for tokens 
 
 void resize_string(String * s, int new_cap) {
     char * new_s = realloc(s->str, new_cap);
@@ -24,7 +34,7 @@ void resize_string(String * s, int new_cap) {
         printf("Could not allocate more memory while resizing string.\n");
         exit(EXIT_FAILURE);
     } 
-    free(s->str);
+
     s->str = new_s;
     s->cap = new_cap;
 }
@@ -40,41 +50,51 @@ void string_add_char(String * s, char c) {
 }
 
 
-void string_add_char(String * s1, String * s2) {    // does not free s2.
-    if (s1->len + s2->len > s1->cap) {
+void string_add_string(String * s1, String * s2) {          // does not free s2.
+    if (s1->len + 1 + s2->len + 1 > s1->cap) {
         resize_string(s1, (s1->len + s2->len) << 1);
     }
 
-    for (int i = 0; i < s2->len; i++) {      // optimize(!)
+    for (int i = 0; i < s2->len; i++) {                     // optimize(!)
         s1->str[s1->len + i] = s2->str[i];
     }
     
     s1->len += s2->len;
-    s1->str[s1->len+1] = '\0';
+    s1->str[s1->len] = '\0';
 }
 
-void init_string(String * s, char * start_string) {
-    s->cap = 4;
-    s->str = malloc(4);
+void init_string(String * s, int cap) {
+    s->cap = cap;
+    s->str = malloc(cap);
     s->len = 0;
-    strcpy_s(s->str, s->cap, start_string);
-    // printf("%s", s->str);
+}
+
+void string_cpy(String * string, char * str, int str_len) {
+    if (str_len+1 > string->cap) {
+        resize_string(string, string->cap << 1);
+    }
+
+    for (int i = 0; i < str_len; i++) {                     // optimize(!)
+        string->str[i] = str[i];
+    }
+    string->len = str_len;
 }
 
 
 int main() {
-    String * s = malloc(sizeof(String));
-    // printf("awdkajwndkjaw\n");
-    // printf("Oh there's sober men a' plenty, And drunkards barely 20!\n");
+    String * s1 = malloc(sizeof(String));
+    String * s2 = malloc(sizeof(String));
     
-    init_string(s, "");
-    for (int i = 0; i < 20; i++){
-        string_add_char(s, '0');
-        printf("%s\n", s->str);
-        printf("%d\n", s->cap);
-    }
-    
+    init_string(s1, 4);
+    init_string(s2, 4);
     
 
-    printf("%s", s->str);
+    string_cpy(s1, "", 0);
+    string_cpy(s2, "123", 3);
+
+    for (int i = 0; i < 20; i++) {
+        string_add_string(s1, s2);
+    }
+
+    exit(EXIT_SUCCESS);
 }
