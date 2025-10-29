411aabe96220a4df7dbc5086f04610bb504421d9
Cys3r0 <cysero.inc@gmail.com>
2025-10-29 23:25:38 +0100

    started work on the next_token()

--- scanner.c
+++ scanner.c
@@ -11,6 +11,11 @@
 //Figure out top-down operator precedence parsing
 
 
+typedef struct {
+    int tokenID;
+    char * str;         // union for str + value (?)
+    int value;
+} Token;
 
 typedef enum {
     ID,
@@ -37,10 +42,39 @@ typedef enum {
     IF,
     WHILE,
     WS,
-} Token;
+} TokenID;
 
 
-char * enum_token_to_str(Token t) {
+char * rules = 
+    "(;)"
+    "|(=)"
+    "|(\\()"
+    "|(\\))"
+    "|(\\{)"
+    "|(\\})"
+    "|(\\[)"
+    "|(\\])"
+    "|(\\+)"
+    "|(\\-)"
+    "|(\\*)"
+    "|(\\/)"
+    "|(%)"
+    "|(==)"
+    "|(!=)"
+    "|(<)"
+    "|(<=)"
+    "|(>)"
+    "|(>=)"
+    "|(if)"
+    "|(while)"
+    "|([a-zA-Z][a-zA-Z0-9]*)"
+    "|([0-9]+)"
+    "|([ \n]+)";
+
+
+
+
+char * enum_token_to_str(TokenID t) {
     switch (t) {
         case ID: return "ID";
         case NUM: return "NUM";
@@ -140,159 +174,142 @@ int str_len(char * str) {
     return i;
 }
 
-void tokenize(String * s, Token * tokens) {
-
-    char * rules = 
-    "(;)"
-    "|(=)"
-    "|(\\()"
-    "|(\\))"
-    "|(\\{)"
-    "|(\\})"
-    "|(\\[)"
-    "|(\\])"
-    "|(\\+)"
-    "|(\\-)"
-    "|(\\*)"
-    "|(\\/)"
-    "|(%)"
-    "|(==)"
-    "|(!=)"
-    "|(<)"
-    "|(<=)"
-    "|(>)"
-    "|(>=)"
-    "|(if)"
-    "|(while)"
-    "|([a-zA-Z][a-zA-Z0-9]*)"
-    "|([0-9]+)"
-    "|([ \n]+)";
-
-    char c;
-    int t_i = 0;
-    
-    Token * t = malloc(1000 * sizeof(Token));
-    
-    
-    regex_t regex;
-    regcomp(&regex, rules, REG_EXTENDED);
-    
-    for (int i = 0; i < s->len; i++) {        
-        regmatch_t m[25];
-        if (regexec(&regex, s->str, 25, m, 0) == 0) {
-            if (m[1].rm_so != -1) { 
-                printf("SEMI\n");
-                tokens[t_i] = SEMI; 
-            }
-            else if (m[2].rm_so != -1) { 
-                printf("ASSIGN\n");
-                tokens[t_i] = ASSIGN; 
-            }
-            else if (m[3].rm_so != -1) { 
-                printf("LPAR\n");
-                tokens[t_i] = LPAR; 
-            }
-            else if (m[4].rm_so != -1) { 
-                printf("RPAR\n");
-                tokens[t_i] = RPAR; 
-            }
-            else if (m[5].rm_so != -1) { 
-                printf("LWING\n");
-                tokens[t_i] = LWING; 
-            }
-            else if (m[6].rm_so != -1) { 
-                printf("RWING\n");
-                tokens[t_i] = RWING; 
-            }
-            else if (m[7].rm_so != -1) { 
-                printf("LBRACKET\n");
-                tokens[t_i] = LBRACKET; 
-            }
-            else if (m[8].rm_so != -1) { 
-                printf("RBRACKET\n");
-                tokens[t_i] = RBRACKET; 
-            }
-            else if (m[9].rm_so != -1) {
-                printf("ADD\n");
-                tokens[t_i] = ADD;
-            }
-            else if (m[10].rm_so != -1) {
-                printf("SUB\n");
-                tokens[t_i] = SUB;
-            }
-            else if (m[11].rm_so != -1) {
-                printf("MUL\n");
-                tokens[t_i] = MUL;
-            }
-            else if (m[12].rm_so != -1) {
-                printf("DIV\n");
-                tokens[t_i] = DIV;
-            }
-            else if (m[13].rm_so != -1) {
-                printf("MOD\n");
-                tokens[t_i] = MOD;
-            }
-            else if (m[14].rm_so != -1) {
-                printf("EQ\n");
-                tokens[t_i] = EQ;
-            }
-            else if (m[15].rm_so != -1) {
-                printf("NEQ\n");
-                tokens[t_i] = NEQ;
-            }
-            else if (m[16].rm_so != -1) {
-                printf("LT\n");
-                tokens[t_i] = LT;
-            }
-            else if (m[17].rm_so != -1) {
-                printf("LEQ\n");
-                tokens[t_i] = LEQ;
-            }
-            else if (m[18].rm_so != -1) {
-                printf("GT\n");
-                tokens[t_i] = GT;
-            }
-            else if (m[19].rm_so != -1) {
-                printf("GEQ\n");
-                tokens[t_i] = GEQ;
-            }
-            else if (m[20].rm_so != -1) {
-                printf("IF\n");
-                tokens[t_i] = IF;
-            }
-            else if (m[21].rm_so != -1) {
-                printf("WHILE\n");
-                tokens[t_i] = WHILE;
-            }
-            else if (m[22].rm_so != -1) { 
-                printf("ID\n");
-                tokens[t_i] = ID; 
-            }
-            else if (m[23].rm_so != -1) { 
-                printf("NUM\n");
-                tokens[t_i] = NUM; 
-            }
-            else if (m[24].rm_so != -1) {
-                printf("WS\n");
-                t_i--;
-                // tokens[t_i] = WS;
-            }
-        }
-        s->str = s->str + (m[0].rm_eo - m[0].rm_so);
-        printf("%s\n", s->str);
-        if (m[0].rm_eo - m[0].rm_so == 0) break; 
-        
-        t_i++;
-    }
+Token * init_token(int token_id, chat * str, int value) {
+    Token * t = malloc(sizeof(Token));
+    t->token_id = token_id;
+    t->str = str;
+    t->value = value;
+    return t;
 }
+
+
+Token * next_token(char ** str, regex_t regex) { // pass regex & m, regex should be initialized somewhere else, main?
+    // NEXT:
+    // FIX so that pointer of pointer is passed (so that str ptr can be increased by size of removed token )
+    // Create a new tokenize method that tokenizes the whole input
+    
+
+    int token_id = -1;
+    char * s = NULL;
+    int value = 0;
+
+    if (regexec(&regex, &str, 25, m, 0) == 0) {
+        int token_len = m[0].rm_eo - m[0].rm_so;
+        if (m[1].rm_so != -1) { 
+            printf("SEMI\n");
+            token_id = SEMI;
+        }
+        else if (m[2].rm_so != -1) { 
+            printf("ASSIGN\n");
+            token_id = ASSIGN;
+        }
+        else if (m[3].rm_so != -1) { 
+            printf("LPAR\n");
+            token_id = LPAR;
+        }
+        else if (m[4].rm_so != -1) { 
+            printf("RPAR\n");
+            token_id = RPAR;
+        }
+        else if (m[5].rm_so != -1) { 
+            printf("LWING\n");
+            token_id = LWING;
+        }
+        else if (m[6].rm_so != -1) { 
+            printf("RWING\n");
+            token_id = RWING;
+        }
+        else if (m[7].rm_so != -1) { 
+            printf("LBRACKET\n");
+            token_id = LBRACKET;
+        }
+        else if (m[8].rm_so != -1) { 
+            printf("RBRACKET\n");
+            token_id = RBRACKET;
+        }
+        else if (m[9].rm_so != -1) {
+            printf("ADD\n");
+            token_id = ADD;
+        }
+        else if (m[10].rm_so != -1) {
+            printf("SUB\n");
+            token_id = SUB;
+        }
+        else if (m[11].rm_so != -1) {
+            printf("MUL\n");
+            token_id = MUL;
+        }
+        else if (m[12].rm_so != -1) {
+            printf("DIV\n");
+            token_id = DIV;
+        }
+        else if (m[13].rm_so != -1) {
+            printf("MOD\n");
+            token_id = MOD;
+        }
+        else if (m[14].rm_so != -1) {
+            printf("EQ\n");
+            token_id = EQ;
+        }
+        else if (m[15].rm_so != -1) {
+            printf("NEQ\n");
+            token_id = NEQ;
+        }
+        else if (m[16].rm_so != -1) {
+            printf("LT\n");
+            token_id = LT;
+        }
+        else if (m[17].rm_so != -1) {
+            printf("LEQ\n");
+            token_id = LEQ;
+        }
+        else if (m[18].rm_so != -1) {
+            printf("GT\n");
+            token_id = GT;
+        }
+        else if (m[19].rm_so != -1) {
+            printf("GEQ\n");
+            token_id = GEQ;
+        }
+        else if (m[20].rm_so != -1) {
+            printf("IF\n");
+            token_id = IF;
+        }
+        else if (m[21].rm_so != -1) {
+            printf("WHILE\n");
+            token_id = WHILE;
+        }
+        else if (m[22].rm_so != -1) { 
+            printf("ID\n");
+            token_id = ID;
+            s = malloc(token_len);
+            for (int i = 0; i < token_len; i++) 
+                s[i] = str[i];  // Optimize!!!!    
+        }
+        else if (m[23].rm_so != -1) { 
+            printf("NUM\n");
+            token_id = NUM;
+            char * value_s = malloc(token_len);
+            for (int i = 0; i < token_len; i++) 
+                value_s[i] = str[i];  // Optimize!!!!    
+            int value = atoi(value_s);
+        }
+        else if (m[24].rm_so != -1) {
+            printf("WS\n");
+            token_id = WS;
+        }
+        str += m[0].rm_eo - m[0].rm_so;
+    }
+
+    return init_token(token_id, s, value);
+}
+
+
 
 int main() {
     String * s = malloc(sizeof(String));
-    Token * t = malloc(1000 * sizeof(Token));
+    TokenID * t = malloc(1000 * sizeof(TokenID));
 
-    init_string(s, 100);
-    
-    
     char * file = "if (abs == 10) print(10000); while (list) {a[]}";
     string_cpy(s, file, str_len(file));
     tokenize(s, t);
